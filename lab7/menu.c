7cae7403cb06243406f553d5c6c0a1ed0af06fc1
Sallybaby <31922219+Sallybaby@users.noreply.github.com>
2017-11-02 21:11:48 +0800

    Update menu.c

--- lab7/menu.c
+++ lab7/menu.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "linktable.h"
 #include "menu.h"
 #define CMD_MAX_LEN 128
 #define DESC_LEN    1024
 #define CMD_NUM     10
 #define CMD_MAX_ARGV_NUM 32
 tLinkTable * head = NULL;
-int Help();
-/* data struct and its operations */
+int Help(int argc, char **argv);
+/* data structure and its operations */
 typedef struct DataNode
 {
     tLinkTableNode * pNext;
     char*   cmd;
     char*   desc;
-    int     (*handler)(int argc, char * argv[]);
+    int     (*handler)(int argc, char **argv);
 } tDataNode;
-int SearchCondition(tLinkTableNode * pLinkTableNode, void * args)
+int SearchCondition(tLinkTableNode * pLinkTableNode, void* args)
 {
-    char * cmd = (char *)args;
+    char* cmd = (char*)args;
     tDataNode * pNode = (tDataNode *)pLinkTableNode;
     if(strcmp(pNode->cmd, cmd) == 0)
-    {
-        return  SUCCESS;  
+   {
+        return  SUCCESS;
     }
-    return FAILURE;           
+    return FAILURE;
 }
 /* find a cmd in the linklist and return the datanode pointer */
-tDataNode* FindCmd(tLinkTable * head, char * cmd)
+tDataNode* FindCmd(tLinkTable * head, char* cmd)
 {
-    return  (tDataNode*)SearchLinkTableNode(head,SearchCondition, (void*)cmd  );
+    return  (tDataNode*)SearchLinkTableNode(head, SearchCondition, cmd);
 }
 /* show all cmd in listlist */
 int ShowAllCmd(tLinkTable * head)
@@ -37,75 +38,74 @@ int ShowAllCmd(tLinkTable * head)
     tDataNode * pNode = (tDataNode*)GetLinkTableHead(head);
     while(pNode != NULL)
     {
-        printf("%s - %s\n", pNode->cmd, pNode->desc);
+        printf("%s\t - \t%s\n", pNode->cmd, pNode->desc);
         pNode = (tDataNode*)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
     }
     return 0;
 }
-int Help(int argc, char * argv[])
+int MenuConfig(char *cmd, char *desc, int (*handler)(int argc, char **argv))
 {
-    ShowAllCmd(head);
-    return 0; 
-}
-int MenuConfig(char * cmd, char * desc, int (* handler)(int argc, char * argv[]) )
-{
+    tDataNode* pNode = NULL;
     if (head == NULL)
     {
-         head = CreateLinkTable();
-         tDataNode* pNode = (tDataNode*)malloc(sizeof(tDataNode));
-         pNode->cmd = "help";
-         pNode->desc = "Menu List:";
-         pNode->handler = Help;
-         AddLinkTableNode(head, (tLinkTableNode *)pNode);
-    }
-    tDataNode* pNode = (tDataNode*)malloc(sizeof(tDataNode));
+        head = CreateLinkTable();
+        pNode = (tDataNode*)malloc(sizeof(tDataNode));
+        pNode->cmd = "help";
+        pNode->desc = "Menu List:";
+        pNode->handler = Help;
+        AddLinkTableNode(head, (tLinkTableNode*)pNode);
+    }    pNode = (tDataNode*)malloc(sizeof(tDataNode));
     pNode->cmd = cmd;
     pNode->desc = desc;
     pNode->handler = handler;
-    AddLinkTableNode(head, (tLinkTableNode *)pNode);
+    AddLinkTableNode(head, (tLinkTableNode*)pNode);
+    return 0;
 }
-int GetArgv(char * cmd, char * argv[], int * argc)
+/* menu program */
+int ExcuteMenu()
 {
-      *argc = 0;
-      char * pcmd = cmd;
-      pcmd = strtok(pcmd, " ");
-      while( pcmd != NULL && *argc <  CMD_MAX_ARGV_NUM)           
-      {
-          argv[*argc] = pcmd;   
-          (*argc)++;
-          pcmd = strtok(NULL, " ");
-      }
-      //remove the last '\n' of the last string
-      int len = strlen( argv[ *argc-1 ] );
-      *(argv[*argc-1] + len -1) = '\0';
-      return 0;
-}
-int ExecuteMenu()
-{
-     while(1)
-     { 
-          int argc = 0;
-          char * argv[CMD_MAX_ARGV_NUM];
-          char cmd[CMD_MAX_LEN];
-          char * pcmd = NULL;
-          printf("Input a cmd number > ");
-          pcmd = fgets(cmd, CMD_MAX_LEN, stdin);
-          if ( pcmd == NULL ) 
-          {
+   /* cmd line begins */
+    while(1)
+    {
+        // init argc
+        int argc = 0;
+        char *argv[CMD_MAX_ARGV_NUM];
+        char cmd[CMD_MAX_LEN];        
+        char *pcmd = NULL;
+        // input cmd
+        printf("Input a cmd > ");
+        pcmd = fgets(cmd, CMD_MAX_LEN, stdin);
+           if (pcmd == NULL)
+            {
               continue;
-          }
-          GetArgv(cmd, argv, &argc);
-          tDataNode *p = FindCmd(head, cmd);
-          if( p == NULL)
-          {
-               printf("This is a wrong cmd!\n ");
-               continue;
-          }
-          printf("%s - %s\n", p->cmd, p->desc); 
-          if(p->handler != NULL) 
-          { 
-              (*(p->handler))(argc, argv);
-          }
+            }
+        // convert cmd line to argc/argv;
+        pcmd = strtok(pcmd, " ");
+        while (pcmd != NULL && argc < CMD_MAX_ARGV_NUM)
+        {
+            argv[argc] = pcmd;
+            ++argc;
+            pcmd = strtok(NULL, " ");
+        }
+        if (argc == 1)
+        {
+            int len = strlen(argv[0]);
+            *(argv[0] + len - 1) = '\0';
+        }
+        tDataNode *p = FindCmd(head, argv[0]);
+        if( p == NULL)
+        {
+            printf("This is a wrong cmd!\n ");
+            continue;
+        }
+        printf("%s\n", p->desc);
+        if(p->handler != NULL)       {
+           p->handler(argc, argv);
+        }
     }
+}
+int Help(int argc, char **argv)
+{
+    ShowAllCmd(head);
     return 0;
 }
