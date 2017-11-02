a20ddfd5988f2d321606fd44c4ee4db67053a03d
Sallybaby <31922219+Sallybaby@users.noreply.github.com>
2017-11-02 21:11:13 +0800

    Update menu.h

--- lab7/menu.h
+++ lab7/menu.h
@@ -1,2 +1,5 @@
-int MenuConfig(char * cmd, char * desc, int (* handler)(int argc, char *argv[]) );
+#ifndef _MENU_H_
+#define _MENU_H_
+int MenuConfig(char *cmd ,char *desc, int (*handler)(int argc ,char *agrv[]));
 int ExecuteMenu();
+#endif
